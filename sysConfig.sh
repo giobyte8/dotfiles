bfa9715a62c1911c2fed9b6071f04f8860a3d515
Giovanni Aguirre <giovanni.fi05@gmail.com>
2016-11-13 04:21:12 -0600

    :computer: Script to automate common config on fresh install

---
+++ sysConfig.sh
@@ -0,0 +1,13 @@
+
+#
+# Config a fresh installed (Debian based) system
+
+# Install java from webupd8team
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+## TODO: Write bash code to execute this task
+## Also ensure your JAVA_HOME variable has been set in '/etc/environment':
+## JAVA_HOME=/usr/lib/jvm/java-8-oracle
+
