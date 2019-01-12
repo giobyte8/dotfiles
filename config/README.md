e4ab2d608a9b2b4d332542c4134c6bbe63447e2b
Giovanni Aguirre <giovanni.fi05@gmail.com>
2019-01-12 01:52:52 -0600

    :rocket: Adding KDE Global shortcuts

---
+++ config/README.md
@@ -0,0 +1,11 @@
+# Config files
+This files should be symlinked from your `~/.config/*` path
+
+## `kglobalshortcutsrc`
+Based on the default KDE config (Taken from KDE Neon 18.04)
+
+Customized shortcuts:
+- Meta + Up -> Maximize window
+- Meta + Down -> Minimize window
+- Meta + Alt + Up -> Maximize vertically
+
---
+++ config/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Symlink the files to current user's '~/.config/' folder and
+# backups original files into '../backups/config/*'
+#
+# @author Giovanni Aguirre
+# @since January 12, 2019
+#
+
+#
+# Backup files from current config
+function backup_current_config {
+    if [ ! -d "../backups/config" ]; then
+        mkdir -p ../backups/config
+    fi
+
+    for file in *; do
+        if [ ! $file = "README.md" ] && [ ! $file = "setup.sh" ]; then 
+            
+            # Check if file exists on active config
+            target="$HOME/.config/$file"
+            backup="../backups/config/$file"
+            if [ -f "$target" ]; then
+                backup="../backups/config/$file"
+                cp $target $backup
+                rm target
+            fi
+      
+            echo "Symlinking: $file"
+            ln -s "./$file" $target
+        fi
+    done
+}
+
+backup_current_config
+
