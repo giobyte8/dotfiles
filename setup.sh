53198f2c0b1204f397f858932220d9ffe6cd2372
Giovanni Aguirre <giovanni.fi05@gmail.com>
2019-01-12 18:38:54 -0600

    :rocket: Add bashrc setup

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Setups the next items:
+# .bashrc
+# .vimrc
+#
+
+function ensure_backup_dir {
+    if [ ! -d "./backups/config" ]; then
+        mkdir -p ./backups/config
+    fi
+}
+
+function setup_bashrc {
+    target_bash="$HOME/.bashrc"
+    
+    # Backup before setup symlink
+    if [ -f "$target_bash"]; then
+        echo "Backing up orginal .bashrc"
+
+        ensure_backup_dir
+        backup="./backups/bashrc"
+        cp $target_bash $backup
+        rm $target
+    fi
+
+    echo "Symlinking .bashrc to: $target_bash"
+    ln -s "$(pwd)/bashrc" $target_bash
+}
+
+setup_bashrc
