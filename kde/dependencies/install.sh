3f5809f792ff882e959b27fd06f98df5daac3629
Giovanni Aguirre <giovanni.fi05@gmail.com>
2019-01-13 23:32:06 -0600

    :rocket: Add logs to install process

--- kde/dependencies/install.sh
+++ kde/dependencies/install.sh
@@ -5,17 +5,21 @@
 #
 
 function _yaml {
+    printf "\nInstalling Kwin YAML\n"
+
     curr_dir="$(pwd)"
     target_dir="/opt/kwin_yaml"
     sudo mkdir -p "$target_dir"
     cd "$target_dir"
 
     # Install required dependencies
+    echo "Verifying dependencies"
     sudo apt-get install -y cmake extra-cmake-modules kwin-dev libdbus-1-dev \
         libkf5config-dev libkf5configwidgets-dev libkf5coreaddons-dev \
         libkf5windowsystem-dev qtbase5-dev
 
     # Clone and install Kwin YAML
+    printf "\nCloning and compiling Kwin YAML\n"
     sudo git clone https://github.com/zzag/kwin-effects-yet-another-magic-lamp.git .
     sudo mkdir build && cd build
     sudo cmake .. \
@@ -23,6 +27,8 @@ function _yaml {
         -DCMAKE_INSTALL_PREFIX=/usr
     sudo make
     sudo make install
+
+    cd "$curr_dir"
 }
 
 _yaml
