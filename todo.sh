3773f15c6a801380c57ced89c2c4ed76ab6426d2
DiganmeGiovanni <giovanni.fi05@gmail.com>
2020-10-27 21:30:36 -0600

    :rocket: Adding a list of some items pending of automation

---
+++ todo.sh
@@ -0,0 +1,11 @@
+# Followindg items are pending of automation
+# Docker
+# Virtual box
+
+# On my fresh Kali install I did:
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install intel-microcode
+sudo apt install mesa-utils
