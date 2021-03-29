af995f05454e7f736222e6a34151ef79c04e1186
Giovanni Aguirre <giovanni.fi05@gmail.com>
2021-03-29 15:47:28 -0600

    :computer:  Add some specific Mac utilities

--- ohmyzsh-custom/aliases.zsh
+++ ohmyzsh-custom/aliases.zsh
@@ -6,3 +6,11 @@ alias dpsap="docker ps -a --format \"table {{.Names}}\t{{.Status}}\t{{.Ports}}\"
 
 # AWS SAM shortcuts
 alias sam-local-api="sam build && sam local start-api"
+
+# Apps shortcuts
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+
+# Quick ssh accesses
+alias rpi="ssh ubuntu@192.168.1.103"
+alias graybox="ssh giovanni@192.168.1.102"
+
--- ohmyzsh-custom/mac_utils.zsh
+++ ohmyzsh-custom/mac_utils.zsh
@@ -1,7 +1,22 @@
+#!/bin/zsh
 
 function dns-set {
-  echo "Setting DNS servers"
-  networksetup -setdnsservers Wi-Fi "$@"
+  if [ $1 = "cloudflare" ]; then
+    echo "Switching to Cloudflare DNS servers"
+    networksetup -setdnsservers Wi-Fi 1.1.1.1 1.0.0.1
+  elif [ $1 = "opendns" ]; then
+    echo "Switching to OpenDNS DNS servers"
+    networksetup -setdnsservers Wi-Fi 208.67.222.222 208.67.220.220
+  elif [ $1 = "google" ]; then
+    echo "Switching to Google DNS servers"
+    networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4
+  elif [ $1 = "pihole" ]; then
+    echo "Switching to pihole as DNS server"
+    networksetup -setdnsservers Wi-Fi 192.168.1.202 1.1.1.1
+  else
+    echo "Setting $@ as DNS servers"
+    networksetup -setdnsservers Wi-Fi "$@"
+  fi
 }
 
 function dns-unset {
@@ -10,8 +25,13 @@ function dns-unset {
 }
 
 function dns-list {
-  echo "Cloudfare: 1.1.1.1 1.0.0.1"
-  echo "OpenDNS:   208.67.222.222 208.67.220.220"
-  echo "Google:    8.8.8.8 8.8.4.4"
+  echo "Some suggested DNS servers:"
+  echo "cloudflare: 1.1.1.1 1.0.0.1"
+  echo "opendns:    208.67.222.222 208.67.220.220"
+  echo "google:     8.8.8.8 8.8.4.4"
+  echo "pihole:     192.168.1.252"
+  echo ""
+  echo "Current DNS:"
+  networksetup -getdnsservers Wi-Fi
 }
 
