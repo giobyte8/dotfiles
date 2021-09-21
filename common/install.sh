117541416f42332eb60c0e243bfa7c2afe2aa987
Giovanni Aguirre <giovanni.fi05@gmail.com>
2021-09-21 10:01:48 -0500

    Initial draft of common setup for ALL OSs

---
+++ common/install.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+# Installs common setup for all *nix systems
+
+
+# ref: https://stackoverflow.com/a/4774063/3211029
+HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+DOTF_ROOT="${HERE}/.."
+BKP_DIR="${DOTF_ROOT}/backup"
+BKP_CNF_DIR="${BKP_DIR}/config"
+
+
+## The exit status of the last command run is 
+## saved automatically in the special variable $?.
+## So, if last command exit status is different
+## than 0, abort execution
+function check_for_errors {
+    if [[ $? > 0 ]]
+    then
+        echo
+        >&2 echo "Aborting installation"
+        exit
+    fi
+}
+
+
+## Ensures existence of config backup directory
+function verify_cnf_bkp_dir {
+    echo
+    echo " Verifying backup directory"
+
+    if [ ! -d "${BKP_CNF_DIR}" ]; then
+        mkdir -p "${BKP_CNF_DIR}"
+
+        # Abort if directory could not be created
+        if [ ! -d "${BKP_CNF_DIR}" ]; then
+            echo ""
+            echo " ERR: Backup dir couldn't be created"
+            return 1
+        fi
+    fi
+}
+
+
+## Try update for 'apt-get' based systems
+function update {
+    if command -v apt-get &> /dev/null
+    then
+        sudo apt-get update
+        check_for_errors
+
+        sudo apt-get upgrade -y
+        check_for_errors
+    else
+        echo
+        echo "apt-get command not found on this system"
+        echo " skipping update"
+    fi
+}
+
+
+## Install and Setup vim
+function setup_vim {
+    echo
+    echo "Setting up vim and Vundle"
+
+    # Verify vim is installed
+    if ! command -v vim &> /dev/null
+    then
+
+        # If not installed, check if apt-get is available to install vim
+        if ! command -v apt-get &> /dev/null
+        then
+            echo
+            echo " vim and apt-get were not found on this system"
+            echo " skipping vim setup"
+
+            return 0
+        fi
+
+        if ! command -v git &> /dev/null
+        then
+            echo
+            echo " Installing git"
+            sudo apt-get install -y git
+            check_for_errors
+        fi
+
+        echo
+        echo " Installing vim"
+        sudo apt-get install -y vim
+        check_for_errors
+    fi
+
+    # Verify vim install check up was successful
+    check_for_errors
+
+    echo
+    echo "Setting up vim and Vundle"
+
+    VIMRC_PATH="${HOME}/.vimrc"
+
+    # TODO Verify VIMRC and BKP_CNF_DIR write permissions
+
+    # Check for preivous symlink existence
+    if [ -L "${VIMRC_PATH}" ]; then
+        echo " WARN: ${VIMRC_PATH} is already a symlink"
+        echo "       $(readlink -f ${VIMRC_PATH})"
+    fi
+
+    # Backup before symlinking
+    if [ -f "${VIMRC_PATH}" ]; then
+        echo " Backing up original .vimrc"
+
+        verify_cnf_bkp_dir
+        check_for_errors
+
+        vimrc_backup="${BKP_CNF_DIR}/vimrc"
+        cp $VIMRC_PATH $vimrc_backup
+        rm $VIMRC_PATH
+    fi
+
+    echo " Symlinking .vimrc"
+    ln -s "${DOTF_ROOT}/config/vimrc" "${VIMRC_PATH}"
+
+    # Install Vundle
+    echo
+    echo " Installing Vundle"
+    git clone \
+        https://github.com/VundleVim/Vundle.vim.git \
+        ~/.vim/bundle/Vundle.vim
+
+    # Install all vim plugins
+    echo
+    echo " Installing plugins"
+}
+
+setup_vim
+## TODO Add source to 'sourceme.sh' file in .bashrc or .zshrc files
---
+++ common/sourceme.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Basic setup compatible with ALL *unix systems
+
+
+##
+## PS1 Variable setup (bash prompt)
+force_color_prompt=yes
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+        # We have color support; assume it's compliant with Ecma-48
+        # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+        # a case would tend to support setf rather than setaf.)
+        color_prompt=yes
+    else
+	    color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    # Color reference: https://unix.stackexchange.com/a/124409/331321
+    PS1='\[\e[033m\] \W \[\e[034m\]> \[\e[m\]'
+    #PS1='\[\033[48;5;178;38;5;232m\] \W \[\e[m\]'
+fi
+unset color_prompt force_color_prompt
+
+## TODO Add source to aliases
+## TODO Add source to custom bash functions
