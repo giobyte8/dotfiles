5c1c9cd8b0910a8bf95eb62747cbe211c7bad796
Giovanni Aguirre <giovanni.fi05@gmail.com>
2021-09-21 10:03:39 -0500

    Update ssh aliases

--- ohmyzsh-custom/aliases.zsh
+++ ohmyzsh-custom/aliases.zsh
@@ -11,6 +11,7 @@ alias sam-local-api="sam build && sam local start-api"
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 
 # Quick ssh accesses
-alias rpi="ssh ubuntu@192.168.1.103"
+alias rpi="ssh rock@192.168.1.103"
 alias graybox="ssh giovanni@192.168.1.102"
+alias meserver="ssh rock@143.244.144.17"
 
