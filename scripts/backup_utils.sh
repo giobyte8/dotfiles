c93e9e513f1527d3228c79d9ee34530b9204e5d0
Giovanni Aguirre <giovanni.fi05@gmail.com>
2022-03-24 17:10:03 -0600

    Add shortcut for backups with rsync

---
+++ scripts/backup_utils.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function backup {
+    if [ ! $# -eq 2 ]; then
+        echo "ERR: Wrong number of arguments"
+        echo " Usage: backup <source/directory/path> <destination/dir/path>"
+        return 1
+    fi
+
+    SRC=$1
+    DST=$2
+
+    if [ ! -d "$SRC" ]; then
+        echo "ERR: Invalid source directory ${SRC}"
+        return 1
+    fi
+
+    if [[ ! $SRC == */ ]]; then
+        SRC="${SRC}/"
+    fi
+
+    echo "Backing up files through rsync:"
+    echo " From: $SRC"
+    echo "   To: $DST"
+
+    rsync --progress -zrh \
+        --ignore-existing \
+        --exclude '*.THM' \
+        --exclude '*.LRV' \
+        $SRC              \
+        $DST
+}
