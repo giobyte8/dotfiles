ebb2a02eef959eddc87a3b9e8812fb0466129870
Giovanni Aguirre <giovanni.fi05@gmail.com>
2022-09-10 16:49:37 -0500

    Isolate mac exclusive init

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # Global 'multi system' configuration init
 
-if [[ $OSTYPE == 'darwin'* ]]; then
+if [ -f "/opt/homebrew/opt/asdf/libexec/asdf.sh" ]; then
+    . /opt/homebrew/opt/asdf/libexec/asdf.sh
+fi
+
+if [ -f "~/.asdf/plugins/java/set-java-home.zsh" ]; then
+    . ~/.asdf/plugins/java/set-java-home.zsh
+    export ASDF_GROOVY_DISABLE_JAVA_HOME_EXPORT=true
+fi
+
+# thrift@0.9 path and flags (Required in mac for skytouch setup)
+if [ -d "/opt/homebrew/opt/thrift@0.9/bin" ]; then
     export PATH="/opt/homebrew/opt/thrift@0.9/bin:$PATH"
     export LDFLAGS="-L/opt/homebrew/opt/thrift@0.9/lib"
     export CPPFLAGS="-I/opt/homebrew/opt/thrift@0.9/include"
