75a6aa6a05073fb32d1f0d1f1ecc52fbc359f3f6
Giovanni Aguirre <giovanni.fi05@gmail.com>
2023-01-19 19:50:43 -0600

    Add support for notifications through borgmatic hooks

---
+++ server/apps/borgmatic/hooks/on_action.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# Executed by borgmatic action hooks
+#
+# Usage:
+# > on_action.sh                    \
+# >   "backups/pictures_repo.borg"  \
+# >   "Photos"                      \
+# >   "LOW"                         \
+# >   "Backup completed"
+#
+# Accepted arguments
+# 1: Repository
+# 2: Backup config file name (provided by borgmatic variables)
+# 3: Notification priority: LOW | MEDIUM | HIGH
+# 4: Action message (Starting backup, Starting prune, etc)
+#
+# Environment configuration
+# Config variables are taken from environment, docker-compose.yml
+# takes care of passing env values.
+#
+# RabbitMQ vhost
+# Currently vhost is always passed as '%2F' wich means '/' (url encoded)
+# This value is not parametrized for now
+#
+
+# For dev purposes only
+if [ -f ../.env ]; then
+    source ../.env
+fi
+
+
+if [ ! $# -eq 4 ]; then
+    echo "ERR: Wrong number of arguments"
+    echo " Usage: on_action.sh <repo> <config> <priority> <msg>"
+    return 1
+fi
+
+# Read and validate arguments
+REPO=$1
+CONF=$2
+PRIORITY=$3
+MESSAGE=$4
+
+
+# Prepare message payload
+
+function json_escape() {
+    printf '%s' "$1" | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))'
+}
+
+msg="$MESSAGE
+Repo: $REPO
+Conf: $CONF"
+j_msg=$(json_escape "$msg")
+
+notification="{
+    \"level\": \"$PRIORITY\",
+    \"message\": $j_msg
+}"
+j_payload=$(json_escape "$notification")
+#payload=$(printf '%s' "$jnotif" | base64)
+
+amqp_msg="{
+    \"properties\": {},
+    \"routing_key\": \"INFRASTRUCTURE_NOTIFICATIONS\",
+    \"payload\": $j_payload,
+    \"payload_encoding\": \"string\"
+}"
+
+
+# Post message to RabbitMQ
+
+curl -s \
+    -u "$RABBITMQ_USER:$RABBITMQ_PASS"  \
+    -X POST                             \
+    -d "$amqp_msg"                      \
+    http://$RABBITMQ_HOST:$RABBITMQ_API_PORT/api/exchanges/%2F/$RABBITMQ_EXCHANGE/publish
