d1fccdf3f815d31adafc6d13b2c0c741bc182089
Giovanni Aguirre <giovanni.fi05@gmail.com>
2023-06-27 04:58:14 +0000

    Replace 'docker-compose' by 'docker compose' in aliases

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -5,7 +5,7 @@ alias dps="docker ps --format \"table {{.Names}}\t{{.Status}}\""
 alias dpsa="docker ps -a --format \"table {{.Names}}\t{{.Status}}\""
 alias dpsp="docker ps --format \"table {{.Names}}\t{{.Status}}\t{{.Ports}}\""
 alias dpsap="docker ps -a --format \"table {{.Names}}\t{{.Status}}\t{{.Ports}}\""
-alias dc="docker-compose"
+alias dc="docker compose"
 
 # AWS SAM shortcuts
 alias sam-local-api="sam build && sam local start-api"
