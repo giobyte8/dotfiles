659e98f9aff1f4d93ca4b53770dcceb51c140efd
Giovanni Aguirre <giovanni.fi05@gmail.com>
2024-10-03 23:03:33 +0000

    Update ssh shortcuts

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -19,13 +19,13 @@ alias dlf="docker logs -f"
 alias sam-local-api="sam build && sam local start-api"
 
 # ssh shortcuts
-alias rpi="ssh rock@192.168.1.103"
+alias rbx="ssh rock@192.168.1.103"
 alias graybox="ssh giovanni@192.168.1.102"
-alias meserver="ssh rock@143.244.144.17"
+alias r1="ssh rock@143.244.144.17"
 
 # ssh shortcuts (Forcing usage of password auth)
 alias sshp="ssh -o PubkeyAuthentication=no -o PreferredAuthentications=password"
-alias rpip="ssh -o PubkeyAuthentication=no -o PreferredAuthentications=password rock@192.168.1.103"
+alias rbxp="ssh -o PubkeyAuthentication=no -o PreferredAuthentications=password rock@192.168.1.103"
 
 # Directories path shortcuts
 alias apps="cd ~/dotf/apps && ls"
