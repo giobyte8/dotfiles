4b2d66f1d80d7f3360db03630f1d1d022a311f26
Giovanni Aguirre <giovanni.fi05@gmail.com>
2024-11-05 09:08:55 -0800

    Quick access to workspace

--- scripts/amex/aliases.sh
+++ scripts/amex/aliases.sh
@@ -2,7 +2,8 @@
 # Setup of alias functions specific to amex env
 
 alias ro="cd ~/src/rooam"
-alias poc="cd ~/src/rooam/1_poc"
+alias poc="cd ~/src/rooam/_poc"
 alias rde="cd ~/src/rooam/docker-env"
+alias ws="cd ~/src/rooam/_poc/workspace"
 
 alias awsr="aws --profile rooam"
