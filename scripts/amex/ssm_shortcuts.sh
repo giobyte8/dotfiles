02aa167de962e889ea96575edee93ab188c61b6e
Giovanni Aguirre <giovanni.fi05@gmail.com>
2025-09-30 14:20:06 -0600

    Improve SSM shortcuts

--- scripts/amex/ssm_shortcuts.sh
+++ scripts/amex/ssm_shortcuts.sh
@@ -1,9 +1,19 @@
 
 # Opens a tunneled connection to jumphost in LZ and
 # maps local port to OpenSearch in AWS
-function opensearch-mdl() {
+function os-mdl() {
     cd ~/src/rooam/rooam-dev-tools/aws
     awsl # Function from 'aws.sh'
 
     bash access-opensearch.sh staging middleware
+}
+
+##
+## Shortcuts to open tunneled connection to RDS Instances
+
+function rds-tpw() {
+    cd ~/src/rooam/rooam-dev-tools/aws
+    awsl # Function from 'aws.sh'
+
+    bash access-rds.sh staging middleware staging-ticket-poll-worker-db-0
 }
