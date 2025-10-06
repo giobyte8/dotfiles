bcbf66d1b1f6ac1133121c30fe90c58e114bbeb9
Giovanni Aguirre <giovanni.fi05@gmail.com>
2025-10-06 11:30:04 -0600

    Load posgres tools if installed via homebrew

--- ohmyzsh-custom/env.zsh
+++ ohmyzsh-custom/env.zsh
@@ -3,3 +3,9 @@ if [ -d "$HOME/.local/bin" ] ; then
     PATH="$HOME/.local/bin:$PATH"
 fi
 
+# Load postgres tools
+#   Installed without installing the full db
+#   See: https://stackoverflow.com/a/49689589/3211029
+if [ -d "/opt/homebrew/opt/libpq/bin" ]; then
+    PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+fi
