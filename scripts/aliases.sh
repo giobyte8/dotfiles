2d4086e9ad44af647706fe68c186796857040c4e
Giovanni Aguirre <giovanni.fi05@gmail.com>
2025-11-30 16:52:09 -0600

    Refresh ssh shortcuts

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -18,7 +18,7 @@ alias dlf="docker logs -f"
 alias sam-local-api="sam build && sam local start-api"
 
 # ssh shortcuts
-alias rbx="ssh rock@192.168.1.103"
+alias rbx="ssh rock@192.168.100.130"
 alias graybox="ssh giovanni@192.168.1.102"
 alias r1="ssh rock@143.244.144.17"
 
