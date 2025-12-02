6cce4ceed31f3a27ee9caae0382ff06379254667
Giovanni Aguirre <giovanni.fi05@gmail.com>
2025-12-02 11:51:56 -0600

    Audit svc db shortcut

--- scripts/amex/ssm_shortcuts.sh
+++ scripts/amex/ssm_shortcuts.sh
@@ -1,16 +1,18 @@
+# Shortcuts to open tunneled connections to jumphost in LZ and
+# map local ports to OpenSearch/RDS in AWS
 
-# Opens a tunneled connection to jumphost in LZ and
-# maps local port to OpenSearch in AWS
+
+# Access open search
 function os-mdl() {
+    # Default to 'staging' if no argument is provided
+    local env="${1:-staging}"
+
     cd ~/src/rooam/rooam-dev-tools/aws
     awsl # Function from 'aws.sh'
-
-    bash access-opensearch.sh staging middleware
+    bash access-opensearch.sh "$env" middleware
 }
 
-##
-## Shortcuts to open tunneled connection to RDS Instances
-
+# Access RDS Aurora
 function rds-tpw() {
     # Default to 'staging' if no argument is provided
     local env="${1:-staging}"
@@ -21,6 +23,27 @@ function rds-tpw() {
         db_name="staging-ticket-poll-worker-db-0"
     elif [[ "$env" == "production" ]]; then
         db_name="production-ticket-poll-worker-db-0"
+    else
+        echo "Unknown environment: $env. Use 'staging' or 'production'."
+        return 1
+    fi
+
+    cd ~/src/rooam/rooam-dev-tools/aws
+    awsl # Function from 'aws.sh'
+
+    bash access-rds.sh "$env" middleware "$db_name"
+}
+
+function rds-audit() {
+    # Default to 'staging' if no argument is provided
+    local env="${1:-staging}"
+
+    # Determine db value based on environment
+    local db_name
+    if [[ "$env" == "staging" ]]; then
+        db_name="staging-middleware-audit-db-0"
+    elif [[ "$env" == "production" ]]; then
+        db_name="production-middleware-audit-db-0"
     else
         echo "Unknown environment: $env. Use 'staging' or 'production'."
         return 1
