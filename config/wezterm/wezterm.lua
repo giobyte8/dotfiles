ba62e9d855681df90045b7bd47d35c690893b7cc
Giovanni Aguirre <giovanni.fi05@gmail.com>
2026-03-30 15:24:23 -0600

    Custom key mappings for word navigation

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -38,6 +38,20 @@ config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
 
 
 -------------------------------------------------------------------------------
+-- Custom key mappings
+
+config.keys = {
+  
+  -- Make Option-Left equivalent to Alt-b which many line editors
+  -- interpret as backward-word
+  { key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb" }},
+  
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  { key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf" }},
+}
+
+
+-------------------------------------------------------------------------------
 -- Others
 
 config.initial_cols = 95
