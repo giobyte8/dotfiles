c3cdc7f063509ed950b695f85ac7d1c047913d6c
Giovanni Aguirre <giovanni.fi05@gmail.com>
2026-04-03 23:53:28 -0600

    Keep disks awake

--- server/utils/storage/keep-disks-awake.sh
+++ server/utils/storage/keep-disks-awake.sh
@@ -2,12 +2,16 @@
 
 # List your mount points here
 MOUNT_POINTS=(
-    /media/vstore
+#    /media/vstore
     /media/redstore1
 )
 
 for MOUNT in "${MOUNT_POINTS[@]}"; do
+
+    # 'mountpoint' checks if directory is a mount point
     if mountpoint -q "$MOUNT"; then
         ls "$MOUNT" >/dev/null 2>&1
+    else
+        echo "Path is not a mountpoint: $MOUNT" >&2
     fi
 done
